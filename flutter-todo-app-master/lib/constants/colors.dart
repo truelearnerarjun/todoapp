4d2cd79506af6df21099b403412abacfbd358fc2
Arjun <arjuntandon82849@gmail.com>
2023-09-18 12:02:25 +0530

    commit

---
+++ flutter-todo-app-master/lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color.fromARGB(255, 10, 87, 106);
+const Color tdBlue = Color.fromARGB(255, 1, 7, 10);
+
+const Color tdBlack = Color.fromARGB(255, 19, 5, 14);
+const Color tdGrey = Color(0xFF717171);
+
+const Color tdBGColor = Color.fromARGB(255, 11, 65, 120);
